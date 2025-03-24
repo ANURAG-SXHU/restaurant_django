2035078c765b305bd67bc6adb1cbb919a1704266
ANURAGSAHUSOUL <anurag.sahu@soulunileaders.com>
2025-03-24 15:52:29 +0530

    Added App

--- Restaurant_Project/Restaurant_Project/urls.py
+++ Restaurant_Project/Restaurant_Project/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
+from Base_App.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('',)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
